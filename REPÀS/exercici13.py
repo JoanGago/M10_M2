afcc2c52982bb32cb37a0781d55cb62a2a5d2bdc
JoanGago <jgago22@jaumebalmes.net>
2024-01-16 18:32:12 +0100

    Joan

--- REPÀS/exercici13.py
+++ REPÀS/exercici13.py
@@ -2,7 +2,7 @@
 #de multiplicar fins el 10. Exemple: l’usuari marca 3, es mostra per pantalla: 3,6,9,12,15,18,21,24,27 i 30
 numero = int(input("Introdueix un numero entre el 1 i 10: "))
 
-if numero < 1 | numero > 10:
+if int(numero) < 1 or int(numero) > 10:
     print("El numeor no esta dintre del rang.")
 else:
     count = 1
---
+++ REPÀS/exercici15.py
@@ -0,0 +1,8 @@
+numeros = []
+
+while True:
+    num = int(input("Introdueix un numero (0 per finalitzar): "))
+    if num == 0:
+        break
+    numeros.append(num)
+
